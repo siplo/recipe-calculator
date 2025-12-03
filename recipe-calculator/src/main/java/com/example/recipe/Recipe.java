879de9050a52e72bee60568650029c52b8036194
Siplak, Michal <msiplak@eservicios.indra.es>
2025-12-03 19:38:35 +0100

    add new recipes

--- recipe-calculator/src/main/java/com/example/recipe/Recipe.java
+++ recipe-calculator/src/main/java/com/example/recipe/Recipe.java
@@ -2,5 +2,5 @@ package com.example.recipe;
 
 import java.util.List;
 
-public record Recipe(String id, String name, double baseWeightKg, List<Ingredient> ingredients) {
+public record Recipe(String id, String name, double baseWeightKg, List<Ingredient> ingredients,String book, String edition, String page) {
 }
